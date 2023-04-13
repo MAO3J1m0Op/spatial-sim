06b8edc0d6a6722c64876cf5d036f4089e7f9e04
Will Harris <woh6@duke.edu>
2023-04-13 08:46:42 -0400

    Create a script to make all of the data folders

---
+++ input_factory.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from input_gen import input_gen
+
+# Constant parameters
+folder_num = 20
+sim_time = 0.5
+count_dumps = 50
+size = 50
+alpha1 = -0.1
+alpha2 = 0.5
+beta1 = 0.6
+beta2 = 0.2
+
+params = (-0.4, -0.2, -0.1, 0.0, 0.1, 0.2, 0.4)
+
+for alpha3 in params:
+    for beta3 in params:
+        input_gen(
+            Path(f'data/{alpha3}.{beta3}'),
+            folder_num, sim_time, count_dumps,
+            (alpha1, alpha2, alpha3),
+            (beta1, beta2, beta3), size) 
--- input_gen.py
+++ input_gen.py
@@ -1,34 +1,42 @@
 from pathlib import Path
+from typing import Tuple
 
-path = Path(input("folder path = "))
-folder_num = int(input("step count = "))
-time_per_sim = float(input("time per sim = "))
-count_dumps = int(input("count dumps per sim = "))
+def input_gen(
+        path: Path, folder_num: int, time_per_sim: float,
+        count_dumps: int, alpha: Tuple[float, float, float],
+        beta: Tuple[float, float, float], lattice_size: int,
+):
+    path.mkdir()
+    input_file = (path / 'input.txt').open('x')
+    count_file = (path / 'count.csv')
 
-path.mkdir()
-input_file = (path / 'input.txt').open('x')
-count_file = (path / 'count.csv')
+    input_file.write(f'init {lattice_size} {alpha[0]} {alpha[1]} {alpha[2]} {beta[0]} {beta[1]} {beta[2]}\n')
 
-alpha = [None] * 3
-beta = [None] * 3
-for i in range(3):
-    alpha[i] = float(input(f"alpha[{i}] = "))
-    beta[i] = float(input(f"beta[{i}] = "))
+    for i in range(folder_num):
+        folder = path / f'img{i}'
+        folder.mkdir()
+        for j in range(count_dumps):
+            input_file.write(f'sim {time_per_sim / count_dumps}\n')
+            input_file.write(f'dump count {count_file}\n')
+        input_file.write(f'dump img {str(folder)}\n')
+        csv = path / f'img{i}.csv'
+        input_file.write(f'dump csv {csv}\n')
 
-size = input("lattice size = ")
+    steps_file = path / 'steps.csv'
+    input_file.write(f'dump steps {str(steps_file)}')
+    input_file.write('exit\n')
 
-input_file.write(f'init {size} {alpha[0]} {alpha[1]} {alpha[2]} {beta[0]} {beta[1]} {beta[2]}\n')
+if __name__ == '__main__':
+    path = Path(input("folder path = "))
+    folder_num = int(input("step count = "))
+    time_per_sim = float(input("time per sim = "))
+    count_dumps = int(input("count dumps per sim = "))
 
-for i in range(folder_num):
-    folder = path / f'img{i}'
-    folder.mkdir()
-    for j in range(count_dumps):
-        input_file.write(f'sim {time_per_sim / count_dumps}\n')
-        input_file.write(f'dump count {count_file}\n')
-    input_file.write(f'dump img {str(folder)}\n')
-    csv = path / f'img{i}.csv'
-    input_file.write(f'dump csv {csv}\n')
+    alpha = [None] * 3
+    beta = [None] * 3
+    for i in range(3):
+        alpha[i] = float(input(f"alpha[{i}] = "))
+        beta[i] = float(input(f"beta[{i}] = "))
+    size = input("lattice size = ")
 
-steps_file = path / 'steps.csv'
-input_file.write(f'dump steps {str(steps_file)}')
-input_file.write('exit\n')
+    input_gen(path, folder_num, time_per_sim, count_dumps, tuple(alpha), tuple(beta), size)
