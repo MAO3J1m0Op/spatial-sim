a77b980f05f479ba32661fab4d2a041157a4b150
Will Harris <woh6@duke.edu>
2023-04-15 18:55:46 -0400

    This just keeps getting worse

--- input_factory.py
+++ input_factory.py
@@ -3,10 +3,10 @@ from pathlib import Path
 from input_gen import input_gen
 
 # Constant parameters
-folder_num = 40
-sim_time = 0.25
+folder_num = 50
+sim_time = 1
 count_dumps = 100
-size = 45
+size = 20
 alpha1 = -0.1
 alpha2 = -0.5
 beta1 = 0.6
@@ -17,7 +17,7 @@ params = (-0.4, -0.2, -0.1, 0.0, 0.1, 0.2, 0.4)
 for alpha3 in params:
     for beta3 in params:
         input_gen(
-            Path(f'data/{alpha3}.{beta3}'),
+            Path(f'data/20x20/{alpha3}.{beta3}'),
             folder_num, sim_time, count_dumps,
             (alpha1, alpha2, alpha3),
             (beta1, beta2, beta3), size) 
